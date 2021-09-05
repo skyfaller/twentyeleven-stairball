0518870c8ec7d3097a4269660536fca66a6efaf2
Nelson Pavlosky <skyfaller@gmail.com>
2021-09-05 12:48:48 -0400

    remove optional HTML

--- content-page.php
+++ content-page.php
@@ -8,23 +8,23 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h2 class="entry-title"><?php the_title(); ?></h2>
+<article id=post-<?php the_ID(); ?> <?php post_class(); ?>>
+	<header class=entry-header>
+		<h2 class=entry-title><?php the_title(); ?></h2>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class=entry-content>
 		<?php the_content(); ?>
 		<?php
 		wp_link_pages(
 			array(
-				'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>',
+				'before' => '<div class=page-link><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>',
 				'after'  => '</div>',
 			)
 		);
 		?>
 	</div><!-- .entry-content -->
-	<footer class="entry-meta">
-		<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
+	<footer class=entry-meta>
+		<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class=edit-link>', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
--- content-single.php
+++ content-single.php
@@ -8,23 +8,23 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h2 class="entry-title"><?php the_title(); ?></h2>
+<article id=post-<?php the_ID(); ?> <?php post_class(); ?>>
+	<header class=entry-header>
+		<h2 class=entry-title><?php the_title(); ?></h2>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
+		<div class=entry-meta>
 			<?php twentyeleven_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class=entry-content>
 		<?php the_content(); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
+		<?php wp_link_pages( array( 'before' => '<div class=page-link><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-meta">
+	<footer class=entry-meta>
 		<?php
 			/* translators: used between list items, there is a space after the comma */
 			$categories_list = get_the_category_list( __( ', ', 'twentyeleven' ) );
@@ -50,18 +50,18 @@
 				esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) )
 			);
 		?>
-		<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
+		<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class=edit-link>', '</span>' ); ?>
 
 		<?php if ( get_the_author_meta( 'description' ) && is_multi_author() ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries ?>
-		<div id="author-info">
-			<div id="author-avatar">
+		<div id=author-info>
+			<div id=author-avatar>
 				<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyeleven_author_bio_avatar_size', 68 ) ); ?>
 			</div><!-- #author-avatar -->
-			<div id="author-description">
+			<div id=author-description>
 				<h2><?php printf( esc_attr__( 'About %s', 'twentyeleven' ), get_the_author() ); ?></h2>
 				<?php the_author_meta( 'description' ); ?>
-				<div id="author-link">
-					<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
+				<div id=author-link>
+					<a href=<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?> rel=author>
 						<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'twentyeleven' ), get_the_author() ); ?>
 					</a>
 				</div><!-- #author-link	-->
