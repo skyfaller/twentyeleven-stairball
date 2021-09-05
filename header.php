3cddec1c5726dc81188eb2cfbae9b203e5dfb1dd
Nelson Pavlosky <skyfaller@gmail.com>
2021-09-05 11:32:03 -0400

    remove IE support + optional HTML, cleanup
    Simplify HTML in preparation for replacing retina.js with img srcset. This may also help get rid of warnings when validating HTML.

--- header.php
+++ header.php
@@ -9,21 +9,10 @@
  * @since Twenty Eleven 1.0
  */
 ?><!DOCTYPE html>
-<!--[if IE 6]>
-<html id="ie6" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 7]>
-<html id="ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html id="ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
 <html <?php language_attributes(); ?>>
-<!--<![endif]-->
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta charset=<?php bloginfo( 'charset' ); ?>>
+<meta name=viewport content="width=device-width, initial-scale=1">
 <title><?php
 	/*
 	 * Print the <title> tag based on what is being viewed.
@@ -43,15 +32,10 @@
 	// Add a page number if necessary:
 	if ( $paged >= 2 || $page >= 2 )
 		echo ' | ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) );
-
 	?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico" />
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
+<link rel=stylesheet href=<?php bloginfo( 'stylesheet_url' ); ?>>
+<link rel=pingback href=<?php bloginfo( 'pingback_url' ); ?>>
+<link rel=icon href=<?php bloginfo('stylesheet_directory'); ?>/favicon.ico>
 <?php
 	/* We add some JavaScript to pages with the comment form
 	 * to support sites with threaded comments (when in use).
@@ -69,43 +53,42 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed">
-	<header id="branding" role="banner">
+<div id=page class=hfeed>
+	<header id=branding>
+		<?php
+			// Check to see if the header image has been removed
+			$header_image = get_header_image();
+			if ( ! empty( $header_image ) ) :
+		?>
+		<a href=<?php echo esc_url( home_url( '/' ) ); ?>>
 			<?php
-				// Check to see if the header image has been removed
-				$header_image = get_header_image();
-				if ( ! empty( $header_image ) ) :
-			?>
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-				<?php
-					// The header image
-					// Check if this is a post or page, if it has a thumbnail, and if it's a big one
-					if ( is_singular() &&
-							has_post_thumbnail( $post->ID ) &&
-							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), array( HEADER_IMAGE_WIDTH, HEADER_IMAGE_WIDTH ) ) ) &&
-							$image[1] >= HEADER_IMAGE_WIDTH ) :
-						// Houston, we have a new header image!
-						echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
-					else : ?>
-					<img src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
-				<?php endif; // end check for featured image or standard header ?>
-			</a>
-			<?php endif; // end check for removed header image ?>
+				// The header image
+				// Check if this is a post or page, if it has a thumbnail, and if it's a big one
+				if ( is_singular() &&
+					has_post_thumbnail( $post->ID ) &&
+					( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), array( HEADER_IMAGE_WIDTH, HEADER_IMAGE_WIDTH ) ) ) &&
+					$image[1] >= HEADER_IMAGE_WIDTH ) :
+					// Houston, we have a new header image!
+					echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
+				else : ?>
+				<img src=<?php header_image(); ?> width=<?php echo HEADER_IMAGE_WIDTH; ?> height=<?php echo HEADER_IMAGE_HEIGHT; ?> alt="">
+			<?php endif; // end check for featured image or standard header ?>
+		</a>
+		<?php endif; // end check for removed header image ?>
 
-			<hgroup>
-				<h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">Stairball<span class="pink">.</span>club</a></span></h1>
-				<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
-			</hgroup>
+		<hgroup>
+			<h1 id=site-title><span><a href=<?php echo esc_url( home_url( '/' ) ); ?> title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">Stairball<span class="pink">.</span>club</a></span></h1>
+			<h2 id=site-description><?php bloginfo( 'description' ); ?></h2>
+		</hgroup>
 
-			<nav id="access" role="navigation">
-				<h3 class="assistive-text"><?php _e( 'Main menu', 'twentyeleven' ); ?></h3>
-				<?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff. */ ?>
-				<div class="skip-link"><a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to primary content', 'twentyeleven' ); ?>"><?php _e( 'Skip to primary content', 'twentyeleven' ); ?></a></div>
-				<div class="skip-link"><a class="assistive-text" href="#secondary" title="<?php esc_attr_e( 'Skip to secondary content', 'twentyeleven' ); ?>"><?php _e( 'Skip to secondary content', 'twentyeleven' ); ?></a></div>
-				<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assiged to the primary position is the one used. If none is assigned, the menu with the lowest ID is used. */ ?>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-			</nav><!-- #access -->
+		<nav id=access>
+			<h3 class=assistive-text><?php _e( 'Main menu', 'twentyeleven' ); ?></h3>
+			<?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff. */ ?>
+			<div class=skip-link><a class=assistive-text href=#content title="<?php esc_attr_e( 'Skip to primary content', 'twentyeleven' ); ?>"><?php _e( 'Skip to primary content', 'twentyeleven' ); ?></a></div>
+			<div class=skip-link><a class=assistive-text href=#secondary title="<?php esc_attr_e( 'Skip to secondary content', 'twentyeleven' ); ?>"><?php _e( 'Skip to secondary content', 'twentyeleven' ); ?></a></div>
+			<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assiged to the primary position is the one used. If none is assigned, the menu with the lowest ID is used. */ ?>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		</nav><!-- #access -->
 	</header><!-- #branding -->
 
-
-	<main id="main">
+	<main id=main>
